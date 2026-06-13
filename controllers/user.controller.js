bf296680a5867979decc5b44e2c2f877dd7766c2
sajid yaqub <sajidmct21@gmail.com>
2026-06-13 12:42:04 +0500

    update the login response

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -79,6 +79,7 @@ export const login = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     id: exsistingUser._id,
     token: token,
+    role:exsistingUser.role,
     apiResponse: new ApiResponse(200, "Logged In", token),
   });
 });
